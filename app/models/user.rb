bbb73ab17bbc459b2cbe7ed90f8a95aea7b11ad0
Divyanshi Raman <ramandivyanshi@gmail.com>
2021-02-21 00:05:02 +0530

    add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+    before_save { email.downcase! }
+    enum role: [:regular_user, :administrator]
     validates :first_name, :last_name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: true, length:{ maximum: 200 }, format: { with: VALID_EMAIL_REGEX }
+    validates :role, presence: true, inclusion: roles.keys
     has_secure_password
     validates :password, presence: true, length: { maximum: 8 }
 end
--- db/migrate/20210219170814_create_users.rb
+++ db/migrate/20210219170814_create_users.rb
@@ -5,7 +5,6 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :password_digest, null: false
-      
       t.timestamps
     end
   end
