a4a362dc3a1c8ae14bc2d70c09a7cf40043d0ffa
Divyanshi Raman <ramandivyanshi@gmail.com>
2021-02-20 23:11:11 +0530

    Fix: syntax errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: true, length:{ maximum: 200 }, format: { with: VALID_EMAIL_REGEX }
     has_secure_password
-    validates: password, presence: true, length: { maximum: 8 }
+    validates :password, presence: true, length: { maximum: 8 }
 end
